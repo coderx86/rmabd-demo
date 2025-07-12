d30969fb6d44578b225d160477bb8367b0063273
azadatiqur <mdatiqurrahman.1010@gmail.com>
2025-07-12 15:59:13 +0600

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,18 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-  title: "RoboMechatronics Association - CUET",
+  title: "RMA",
   description: "Official website of RoboMechatronics Association at CUET",
-  generator: "v0.dev",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
